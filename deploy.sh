4a002789f8a729c122e8ff61f051540bdd3bdf9b
pierreozoux <pierre@ozoux.net>
2018-08-16 15:02:56 +0200

    Adds scripts.

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -eux
+
+aws s3 cp ./ s3://ocp-quickstart/quickstart-redhat-openshift --recursive --exclude ".git/*"
+
+aws cloudformation deploy \
+  --template-file ./templates/openshift.template \
+  --s3-bucket ocp-quickstart \
+  --s3-prefix deploy \
+  --capabilities CAPABILITY_IAM \
+  --stack-name $1 \
+  --parameter-overrides \
+      KeyPairName= \
+      PrivateSubnet1ID= \
+      PrivateSubnet2ID= \
+      PrivateSubnet3ID= \
+      PublicSubnet1ID= \
+      PublicSubnet2ID= \
+      PublicSubnet3ID= \
+      QSS3BucketName=ocp-quickstart \
+      RemoteAccessCIDR=10.0.0.0/8 \
+      ContainerAccessCIDR=10.0.0.0/8 \
+      VPCID= \
+      VPCCIDR=10.104.220.0/22 \
+      OpenShiftAdminPassword= \
+      RedhatSubscriptionUserName= \
+      RedhatSubscriptionPassword= \
+      RedhatSubscriptionPoolID= \
+      OpenshiftContainerPlatformVersion=3.10 \
+      MasterInstanceType=t2.large \
+      EtcdInstanceType=t2.large \
+      NodesInstanceType=t2.large \
+      NumberOfNodes=1 \
+      ClusterName=$1 \
+      DomainName= \
+      AWSServiceBroker=Disabled \
+      HawkularMetrics=Disabled
